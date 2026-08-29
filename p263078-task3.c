2f7adb5e7736463fb5d4ff7fa042afb83761d5e2
mehwish-523 <mehwishnawaz523@gmail.com>
2026-08-29 02:10:50 -0800

    Add files via upload

---
+++ p263078-task3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+printf("=============== C ESCAPE SEQUENCES REFRENCE ===============\n");
+printf("1.  To print a double quote,write:\t\\\"\n");
+printf("2.  To print a literal backslash,write:\t\\\\\n");
+printf("3.  To insert a newline break,write:\t\\n\n");
+printf("4.  To insert a horizontal tab stop, write:\\t\n");
+printf("5.  To print a percentage sign,write:        %%%%\n");
+printf("===========================================================\n");
+return 0;
+
+}
