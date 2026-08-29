88a5ec22d49bcb26f598d2bae91e9f9c6289c380
mehwish-523 <mehwishnawaz523@gmail.com>
2026-08-29 01:27:04 -0800

    Add files via upload

---
+++ p263078_task2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int Year=2026;
+char Section='B';
+float TestScore=61.25;
+double SemesterFee=235622.40;
+printf("=====================================================\n");
+printf("\t\tFAST-NUCES STUDENT PROFILE CARD\n");
+printf("=====================================================\n");
+printf("Batch Year:  \t%d  \n",Year);
+printf("Section:\t%c\n",Section);
+printf("Test Score:\t%.2f  \n",TestScore);
+printf("Semester Fee:\tPKR %.2lf  \n",SemesterFee);
+printf("Motto:\t\t\"Small daily steps lead to big success\"\n");
+printf("======================================================\n");
+return 0;
+}
